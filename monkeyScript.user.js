3c55ef48b74da73517606a5b09d5a35d4f1d2ae2
yuiffy <yuiffy@qq.com>
2020-04-11 01:43:27 +0800

    更新，使各语言都变中文

--- monkeyScript.user.js
+++ monkeyScript.user.js
@@ -1,18 +1,17 @@
 // ==UserScript==
-// @name         godfield.net汉化
+// @name         godfield.net Chinese Translate
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  make godfield language "pt" to Chinese!
+// @version      0.1.1
+// @description  make godfield.net to Chinese. (ps. need to open a new page, F5 will become old languages)
 // @supportURL   https://gitee.com/skydog/god-field-skydog-plugin
 // @author       237th
-// @match        https://godfield.net/?lang=pt
+// @match        *://godfield.net/*
 // @resource outJson http://skydog.gitee.io/god-field-skydog-plugin/cn.json
 // @grant           GM_getResourceText
-// @grant           unsafeWindow
 // @run-at         document-start
 // ==/UserScript==
 
-(function (window) {
+(async function () {
   'use strict';
 
   const myOut = GM_getResourceText("outJson");
@@ -21,23 +20,30 @@
   var open_prototype = XMLHttpRequest.prototype.open,
     intercept_response = function (urlpattern, callback) {
       XMLHttpRequest.prototype.open = function () {
-        arguments['1'].match(urlpattern) && this.addEventListener('readystatechange', function (event) {
-          if (this.readyState === 4) {
+        if (arguments['1'].match(urlpattern)) {
+          console.log("open", arguments['1']);
+          function doHack() {
             var response = callback(event.target.responseText);
             Object.defineProperty(this, 'response', {writable: true});
             Object.defineProperty(this, 'responseText', {writable: true});
             this.response = this.responseText = response;
           }
-        });
+
+          this.addEventListener('readystatechange', function (event) {
+            if (this.readyState === 4) {
+              doHack.call(this);
+            } else {
+              console.log("readystatechange", this.readyState);
+            }
+          });
+        }
         return open_prototype.apply(this, arguments);
       };
     };
 
-  intercept_response(/pt\.json/i, function (response) {
-    console.log("response will change", response, myOut);
+  intercept_response(/(pt|ja|en)\.json/i, function (response) {
+    console.log("response will change!");
     var new_response = myOut;
     return new_response;
   });
-
-
-})(unsafeWindow);
+})();
